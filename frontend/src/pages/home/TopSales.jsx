978cd11eb0f3d3b450877d223867c3a38264ad70
Azara <yyushahu02@gmail.com>
2024-10-29 19:48:55 +0000

    finalise top sellers section using swiperjs for nav/scroll effect

--- frontend/src/pages/home/TopSales.jsx
+++ frontend/src/pages/home/TopSales.jsx
@@ -1,6 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import BookCard from '../books/BookCard';
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
 const categories = ["Choose a genre", "Business", "Marketing", "Fiction", "Horror", "Adventure", "Books"];
 
 const TopSales = () => {
@@ -24,7 +35,7 @@ const TopSales = () => {
       <div className='mb-8 flex items-center'>
         <select
           onChange={(e) => setSelectedCategory(e.target.value)}
-          name="category" id="_category" className='border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none'>
+          name="category" id="_category" className='font-primary border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none'>
           {
             categories.map((category, index) => (
               <option value={category} key={index} className='bg-[#EAEAEA]'>{category}</option>
@@ -33,11 +44,40 @@ const TopSales = () => {
         </select>
       </div>
 
-      {
-        filteredBooks.map((book, index) => (
-          <BookCard key={index} book={book} />
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {
+        filteredBooks.length > 0 && filteredBooks.map((book, index) => (
+          <SwiperSlide key={index}>
+            <BookCard book={book} />
+          </SwiperSlide>
+
         ))
       }
+      </Swiper>
     </div>
   )
 }
